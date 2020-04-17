25dcf1f0b721f454b82003376997346a03a3a98d
kraven <ravliv.pavlobohdan@gmail.com>
2020-04-17 11:26:52 +0200

    first square

---
+++ src/Renderer/Model.java
@@ -0,0 +1,35 @@
+package Renderer;
+
+/**
+ * Represents a loaded model. It contains the ID of the VAO that contains the
+ * model's data, and holds the number of vertices in the model.
+ *
+ * @author Karl
+ *
+ */
+public class Model {
+
+    private int vaoID;
+    private int vertexCount;
+
+    public Model(int vaoID, int vertexCount) {
+        this.vaoID = vaoID;
+        this.vertexCount = vertexCount;
+    }
+
+    /**
+     * @return The ID of the VAO which contains the data about all the geometry
+     *         of this model.
+     */
+    public int getVaoID() {
+        return vaoID;
+    }
+
+    /**
+     * @return The number of vertices in the model.
+     */
+    public int getVertexCount() {
+        return vertexCount;
+    }
+
+}
---
+++ src/shaders/ShaderProgram.java
@@ -0,0 +1,109 @@
+package shaders;
+
+import org.lwjgl.BufferUtils;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL20;
+import org.lwjgl.system.MemoryUtil;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.FloatBuffer;
+
+import static org.lwjgl.opengl.GL20.*;
+
+
+public class ShaderProgram
+{
+
+    private final int programId;
+
+    private int vertexShaderId;
+
+    private int fragmentShaderId;
+
+    public ShaderProgram() throws Exception {
+        programId = glCreateProgram();
+        if (programId == 0) {
+            throw new Exception("Could not create Shader");
+        }
+    }
+    public void createVertexShader(String shaderCode) throws Exception {
+        vertexShaderId = createShader(shaderCode, GL_VERTEX_SHADER);
+    }
+
+    public void createFragmentShader(String shaderCode) throws Exception {
+        fragmentShaderId = createShader(shaderCode, GL_FRAGMENT_SHADER);
+    }
+
+    protected int createShader(String shaderCode, int shaderType) throws Exception {
+        int shaderId = glCreateShader(shaderType);
+        if (shaderId == 0) {
+            throw new Exception("Error creating shader. Type: " + shaderType);
+        }
+
+        StringBuilder shaderSource = new StringBuilder();
+        try{
+            BufferedReader reader = new BufferedReader(new FileReader(shaderCode));
+            String line;
+            while((line = reader.readLine())!=null){
+                shaderSource.append(line).append("//\n");
+            }
+            reader.close();
+        }catch(IOException e){
+            e.printStackTrace();
+            System.exit(-1);
+        }
+        glShaderSource(shaderId, shaderSource);
+        glCompileShader(shaderId);
+
+//        if (glGetShaderi(shaderId, GL_COMPILE_STATUS) == 0) {
+//            throw new Exception("Error compiling Shader code: " + glGetShaderInfoLog(shaderId, 1024));
+//        }
+
+        if(GL20.glGetShaderi(shaderId, GL20.GL_COMPILE_STATUS )== GL11.GL_FALSE){
+            System.out.println(GL20.glGetShaderInfoLog(shaderId, 500));
+            System.err.println("Could not compile shader!");
+            System.exit(-1);
+        }
+
+        glAttachShader(programId, shaderId);
+
+        return shaderId;
+    }
+
+    public void link() throws Exception {
+        glLinkProgram(programId);
+        if (glGetProgrami(programId, GL_LINK_STATUS) == 0) {
+            throw new Exception("Error linking Shader code: " + glGetProgramInfoLog(programId, 1024));
+        }
+
+        if (vertexShaderId != 0) {
+            glDetachShader(programId, vertexShaderId);
+        }
+        if (fragmentShaderId != 0) {
+            glDetachShader(programId, fragmentShaderId);
+        }
+
+        glValidateProgram(programId);
+        if (glGetProgrami(programId, GL_VALIDATE_STATUS) == 0) {
+            System.err.println("Warning validating Shader code: " + glGetProgramInfoLog(programId, 1024));
+        }
+
+    }
+
+    public void bind() {
+        glUseProgram(programId);
+    }
+
+    public void unbind() {
+        glUseProgram(0);
+    }
+
+    public void cleanup() {
+        unbind();
+        if (programId != 0) {
+            glDeleteProgram(programId);
+        }
+    }
+}
