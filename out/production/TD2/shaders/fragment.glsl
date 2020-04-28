d871f06e88a0c100f697c6a48019c28c28c735e8
kraven <ravliv.pavlobohdan@gmail.com>
2020-04-28 11:58:36 +0200

    opa tilky src

--- out/production/TD2/shaders/fragment.glsl
+++
@@ -1,9 +0,0 @@
-#version 130
-
-in vec3 myPos;
-out vec4 fragColor;
-
-void main(void)
-{
-    fragColor = vec4( normalize(myPos).y > 0.1f ? vec3(0.0f, 0.0f, 0.0f) : vec3(0.0f, 1.0f, 0.93f), 1.0);
-}
